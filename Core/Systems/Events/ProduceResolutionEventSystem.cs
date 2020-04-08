7e8f3390d839bf972da30528045dd33875a02bf9
Porrith Suong <psuong95@gmail.com>
2020-04-08 16:15:23 -0400

    Prototypes with Jeff Vella's event system.

---
+++ Core/Systems/Events/ProduceResolutionEventSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using Vella.Events;
+
+namespace UGUIDots.Events.Systems {
+
+    [AlwaysUpdateSystem]
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    public class SpawnResolutionEventSystem : SystemBase {
+
+        private int2 resolution;
+        private EventQueue<ResolutionEvent> eventQueue;
+
+        protected override void OnCreate() {
+            resolution = new int2(Screen.width, Screen.height);
+
+            eventQueue = World.GetOrCreateSystem<EntityEventSystem>().GetQueue<ResolutionEvent>();
+        }
+
+        protected override void OnUpdate() {
+            var current = new int2(Screen.width, Screen.height);
+
+            if (!current.Equals(resolution)) {
+                eventQueue.Enqueue(new ResolutionEvent {
+                    Value = current
+                });
+
+                resolution = current;
+            }
+        }
+    }
+}
